4d670e516086a7823008902b6ecbd15f91262a10
Patrick Kinsella <pkinsella1@gmail.com>
2016-09-25 13:40:19 -0400

    Renamed project. Added .gitignore

--- fortune_teller.rb
+++ fortune_teller.rb
@@ -1,16 +1,14 @@
 # Patrick Kinsella
-# Silly program to take all roommate names and print them with random
-# adjectives to the terminal so you can laugh with roomies.
+# Print names and print them with random
+# adjectives to the terminal so you can laugh with friends.
 # 
-# Takes a named file with a list of current roommates
-#
 # Primary goal is to learn git
 #
 # Created Sat, 4/16/2016
 
 class SassMachine
   def initialize
-    @roommates = ["Dennis", "Annie", "Alli", "Theresa", "Darby", "Allison", \
+    @sass_targets = ["Dennis", "Annie", "Alli", "Theresa", "Darby", "Allison", \
                   "Ilana", "Patrick"]
     @choice
     @adjectives = ["is a wily old dog", "is a witty kitty", "is a sass machine", \
@@ -18,12 +16,12 @@ class SassMachine
     @prng = Random.new
   end
   
-  def choose_roommate()
-    puts "please select the roommate you wish to describe."
+  def choose_sass_targets()
+    puts "please select the person you wish to describe."
  
     i = 0
  
-    @roommates.each do |person, index|
+    @sass_targets.each do |person, index|
       i += 1
       print "#{i}. "
       puts "#{person}"
@@ -36,14 +34,14 @@ class SassMachine
 
   def sass
     puts
-    puts "#{@roommates[@choice]} #{@adjectives[@prng.rand(6)]}"
+    puts "#{@sass_targets[@choice]} #{@adjectives[@prng.rand(6)]}"
   end
 end
 
 
 def main
   beacon = SassMachine.new()
-  beacon.choose_roommate  
+  beacon.choose_sass_targets 
   beacon.sass
 end
 
