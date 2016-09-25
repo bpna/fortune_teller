38d8afc4e9555f058a0868d093378f65a3a5383e
Patrick Kinsella <pkinsella1@gmail.com>
2016-09-25 12:25:44 -0400

    Add README. Condense roommate choice, descriptors, and output into a class. Add selection for which roommate to describe

---
+++ README.md
@@ -0,0 +1,6 @@
+The roommate printer is a program that shall demonstrate the wonderful and amazing
+qualities of artificial prose. It does not discriminate in its praise or in its
+criticism of all human beings. We are all equal under the omnipresent eye of
+the machine and its perverse proclivities.
+
+Created by Patrick Kinsella on 9/24/2016
--- roomprint.rb
+++ roomprint.rb
@@ -8,23 +8,43 @@
 #
 # Created Sat, 4/16/2016
 
-def main
-  roomies = Array.new
-  adjectives = ["is a wily old dog", "is a witty kitty", "is a sass machine", \
+class SassMachine
+  def initialize
+    @roommates = ["Dennis", "Annie", "Alli", "Theresa", "Darby", "Allison", \
+                  "Ilana", "Patrick"]
+    @choice
+    @adjectives = ["is a wily old dog", "is a witty kitty", "is a sass machine", \
               "is a goony loony", "is a boss with sauce", "is a boney crony"]
-  i = 0
-
-  ARGF.each do |line|
-    roomies[i] = line.chop
-    i += 1
+    @prng = Random.new
+  end
+  
+  def choose_roommate()
+    puts "please select the roommate you wish to describe."
+ 
+    i = 0
+ 
+    @roommates.each do |person, index|
+      i += 1
+      print "#{i}. "
+      puts "#{person}"
+    end
+  
+    puts
+    @choice = $stdin.gets.chomp.to_i
+    @choice -= 1
   end
 
-  prng = Random.new
-
-  roomies.each do |roomie|
-    puts "#{roomie} #{adjectives[prng.rand(6)]}"
+  def sass
+    puts
+    puts "#{@roommates[@choice]} #{@adjectives[@prng.rand(6)]}"
   end
+end
 
+
+def main
+  beacon = SassMachine.new()
+  beacon.choose_roommate  
+  beacon.sass
 end
 
 main()
